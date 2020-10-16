34c614fca11329ec018a5b4388dbc91b8472b608
Tom Parker <tpar2000@yahoo.co.uk>
2020-10-16 23:03:38 +0100

    done

--- application.py
+++ application.py
@@ -9,7 +9,7 @@ stats_helper = StatsHelper()
 @app.route('/')
 def homepage():
     # HINT: Pass variables through to the HTML using Flask - https://flask.palletsprojects.com/en/1.1.x/quickstart/#rendering-templates
-    return render_template('index.html', avg_r = get_average_overall_rating(), top_five = [get_top_five()])
+    return render_template('index.html', avg_r = get_average_overall_rating(), top_five = [get_top_five()], best_style = get_best_appearance(), avg_t = get_avg_taste(), avg_p = get_avg_palate())
 
 # HINT: This could be your first statistic!
 def get_average_overall_rating():
@@ -17,3 +17,13 @@ def get_average_overall_rating():
 
 def get_top_five():
     return stats_helper.top_five_beers()
+
+
+def get_best_appearance():
+    return stats_helper.calculate_best_style()
+
+def get_avg_taste():
+    return stats_helper.calc_avg_taste()
+
+def get_avg_palate():
+    return stats_helper.calc_avg_palate()
--- helpers/stats_helper.py
+++ helpers/stats_helper.py
@@ -11,9 +11,20 @@ class StatsHelper():
         result = self.database.fetch_one("SELECT AVG(review_overall) FROM reviews")
         return result[0]
 
+    def calculate_best_style(self):
+        result = self.database.fetch_one("SELECT beer_name FROM reviews GROUP BY beer_name ORDER BY AVG(review_appearance) DESC LIMIT 1")
+        return result[0]
+        
+    def calc_avg_taste(self):
+        result = self.database.fetch_one("SELECT AVG(review_taste) FROM reviews")
+        return result[0]
+    def calc_avg_palate(self):
+        result = self.database.fetch_one("SELECT AVG(review_palate) FROM reviews")
+        return result[0]
+
     def top_five_beers(self):
         result = self.database.fetch_all("SELECT beer_name, AVG(review_taste) FROM reviews GROUP BY beer_name ORDER BY AVG(review_taste) DESC LIMIT 5")
-        return result[0]    
+        return result
         
     def worst_five_beers(self):
         result = self.database.fetch_all("SELECT beer_name, AVG(review_overall), AVG(review_taste) FROM reviews GROUP BY beer_name ORDER BY review_overall ASC LIMIT 5")
