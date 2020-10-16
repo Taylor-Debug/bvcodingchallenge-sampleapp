ad10585a4a71181b0c707df5f7ef8a9c58adbd07
Tom Parker <tpar2000@yahoo.co.uk>
2020-10-16 21:12:49 +0100

    sql queries

--- helpers/stats_helper.py
+++ helpers/stats_helper.py
@@ -11,6 +11,14 @@ class StatsHelper():
         result = self.database.fetch_one("SELECT AVG(review_overall) FROM reviews")
         return result[0]
 
+    def top_five_beers(self):
+        result = self.database.fetch_one("SELECT beer_name, AVG(review_taste) FROM reviews GROUP BY beer_name ORDER BY review_taste DESC LIMIT 5")
+        return result[0]    
+        
+    def worst_five_beers(self):
+        result = self.database.fetch_one("SELECT beer_name, AVG(review_overall), AVG(review_taste) FROM reviews GROUP BY beer_name ORDER BY review_overall ASC LIMIT 5")
+        return result[0]    
+
     # HINT: You can define more queries here, along with some python logic to calculate!
     def calculate_another_stat(self):
       # all_rows = self.database.fetch_all("")
