c67abfa7d94dadc2621522352772f5a4f39da800
Tom Parker <tpar2000@yahoo.co.uk>
2020-10-16 19:55:13 +0100

    test

--- application.py
+++ application.py
@@ -9,7 +9,7 @@ stats_helper = StatsHelper()
 @app.route('/')
 def homepage():
     # HINT: Pass variables through to the HTML using Flask - https://flask.palletsprojects.com/en/1.1.x/quickstart/#rendering-templates
-    return render_template('index.html', todays_date=datetime.now())
+    return render_template('index.html', todays_date=datetime.now, content = StatsHelper.caculate_ave_overall_rating(self))
 
 # HINT: This could be your first statistic!
 def get_average_overall_rating():
