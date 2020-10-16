29e5af2205fd1c65ba6bd42501e41160923540d8
Tom Parker <tpar2000@yahoo.co.uk>
2020-10-16 22:45:03 +0100

    TABLE

--- application.py
+++ application.py
@@ -9,8 +9,11 @@ stats_helper = StatsHelper()
 @app.route('/')
 def homepage():
     # HINT: Pass variables through to the HTML using Flask - https://flask.palletsprojects.com/en/1.1.x/quickstart/#rendering-templates
-    return render_template('index.html', avg_r = get_average_overall_rating())
+    return render_template('index.html', avg_r = get_average_overall_rating(), top_five = [get_top_five()])
 
 # HINT: This could be your first statistic!
 def get_average_overall_rating():
     return stats_helper.caculate_ave_overall_rating()
+
+def get_top_five():
+    return stats_helper.top_five_beers()
--- helpers/stats_helper.py
+++ helpers/stats_helper.py
@@ -12,17 +12,9 @@ class StatsHelper():
         return result[0]
 
     def top_five_beers(self):
-        result = self.database.fetch_one("SELECT beer_name, AVG(review_taste) FROM reviews GROUP BY beer_name ORDER BY review_taste DESC LIMIT 5")
+        result = self.database.fetch_all("SELECT beer_name, AVG(review_taste) FROM reviews GROUP BY beer_name ORDER BY AVG(review_taste) DESC LIMIT 5")
         return result[0]    
         
     def worst_five_beers(self):
-        result = self.database.fetch_one("SELECT beer_name, AVG(review_overall), AVG(review_taste) FROM reviews GROUP BY beer_name ORDER BY review_overall ASC LIMIT 5")
+        result = self.database.fetch_all("SELECT beer_name, AVG(review_overall), AVG(review_taste) FROM reviews GROUP BY beer_name ORDER BY review_overall ASC LIMIT 5")
         return result[0]    
-
-    # HINT: You can define more queries here, along with some python logic to calculate!
-    def calculate_another_stat(self):
-      # all_rows = self.database.fetch_all("")
-      return None
-
-    def myMethod_test(self):
-       result = self.database.fetch_one
